e4ecc074d25d60a8ed5a06f76f2c8d86440c6abd
ferrycyna <fer.irawan220@gmail.com>
2024-05-05 23:36:50 +0700

    conection

--- connection.js
+++ connection.js
@@ -2,9 +2,9 @@ const mysql = require("mysql");
 
 const db = mysql.createConnection({
     host : "sql6.freesqldatabase.com",
-    user : "sql6702772",
-    database : "sql6702772",
-    password : "z1y63UXxGk",
+    user : "sql6704238",
+    database : "sql6704238",
+    password : "4p329gIHIp",
 });
 
 module.exports = db;
